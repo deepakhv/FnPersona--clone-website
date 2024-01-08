c47cf26cce1080f8ba433dd5956f2756a47e5f2a
deepakhv <deepakdeepu29052002@gmail.com>
2024-01-08 17:15:43 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Navbar from "./Navbar"
+import Data from "./Data"
+
+export default function App(){
+    return(
+        <div>
+            <Navbar />
+            <Data />
+        </div>
+    )
+}
---
+++ Navbar.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Navbar() {
+    return(
+        <div className="navbar">
+            <div className="logo">
+                <img src="https://fnpersona.netlify.app/fn_logo.png"/>
+                <span>FnPersona</span>
+            </div>
+            <div className="contact">
+                <button className="btn">Babylon</button>
+                <img src="https://cdn-icons-png.flaticon.com/128/9581/9581121.png" />
+            </div>
+        </div>
+    )
+}
